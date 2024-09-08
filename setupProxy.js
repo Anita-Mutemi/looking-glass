bc35a3a3795c0a873d32bd83ae7a405bd49196c9
Anita-Mutemi <anitajehu13@gmail.com>
2024-09-08 20:11:40 +0300

    add crm link

--- setupProxy.js
+++ setupProxy.js
@@ -4,7 +4,7 @@ export default function (app) {
   app.use(
     ['/', '/login', '/auth', '/v1', '/projects', '/me', '/v1/projects'],
     proxy({
-      target: 'https://crmv2.alphaterminal.pro',
+      target: 'https://desolate-harbor-30841-707d8d470803.herokuapp.com',
       changeOrigin: true,
     }),
   );
